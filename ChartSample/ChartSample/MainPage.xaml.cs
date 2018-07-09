cfafac8c1cee816e40bca8bbbac7e19527c5dc65
Sivaram ganesan <sivaram.ganesan@syncfusion.com>
2018-07-09 14:30:42 +0530

    First commit

---
+++ ChartSample/ChartSample/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.SfNavigationDrawer.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChartSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public SfNavigationDrawer Drawer
+        {
+			get;
+			set;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            Drawer = new SfNavigationDrawer()
+            {
+                DrawerWidth = 400
+			};
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,38 @@
+﻿using ChartSample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamarin.Forms;
+
+namespace NUnitTestProject
+{
+    [TestClass]
+    public class Test
+    {
+        [TestInitialize()]
+        public void SetUp()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MDODAwQDMxMzYyZTMyMmUzMFJDRjN6TjlNaEIzcUJRN09JbUdyczNCcDRxS2c2eVFndS9neTYzZXBsaTQ9");
+            Xamarin.Forms.Mocks.MockForms.Init();
+        }
+
+        ContentPage page;
+        ContentPage CreateInstance()
+        {
+            page = new MainPage();
+            return page;
+        }
+
+        [TestMethod]
+        public void TestCase()
+        {
+            CreateInstance();
+            Assert.IsNotNull(page);
+        }
+
+        [TestMethod]
+        public void TestCase1()
+        {
+            CreateInstance();
+            Assert.AreEqual((page as MainPage).Drawer.DrawerWidth, 400);
+        }
+    }
+}
